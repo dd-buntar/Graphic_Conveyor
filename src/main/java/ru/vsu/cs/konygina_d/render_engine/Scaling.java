bb32b202eab10e379f4515ee26d471ee033f41ae
dd-buntar <ckonyginad@yandex.ru>
2024-12-17 13:52:40 +0300

    refactor Translator and Scaling

--- src/main/java/ru/vsu/cs/konygina_d/render_engine/Scaling.java
+++ src/main/java/ru/vsu/cs/konygina_d/render_engine/Scaling.java
@@ -2,40 +2,42 @@ package ru.vsu.cs.konygina_d.render_engine;
 
 import io.github.alphameo.linear_algebra.mat.Mat4;
 import io.github.alphameo.linear_algebra.mat.Matrix4;
+import static io.github.alphameo.linear_algebra.mat.Matrix4Col.*;
+import static io.github.alphameo.linear_algebra.mat.Matrix4Row.*;
 import io.github.alphameo.linear_algebra.vec.Vec3;
 import io.github.alphameo.linear_algebra.vec.Vector3;
 import java.util.Objects;
 
 public class Scaling implements AffineTransformation {
-    private final float Sx, Sy, Sz;
+    private final Matrix4 scaleMatrix;
 
     public Scaling(float sx, float sy, float sz) {
-        Sx = sx;
-        Sy = sy;
-        Sz = sz;
+        this.scaleMatrix = new Mat4(
+                sx, 0, 0, 0,
+                0, sy, 0, 0,
+                0, 0, sz, 0,
+                0, 0, 0, 1);
     }
 
     public Scaling() {
-        Sx = 1;
-        Sy = 1;
-        Sz = 1;
+        this.scaleMatrix = new Mat4(
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1);
     }
 
     @Override
     public Matrix4 getMatrix() {
-        return new Mat4(
-                Sx, 0, 0, 0,
-                0, Sy, 0, 0,
-                0, 0, Sz, 0,
-                0, 0, 0, 1);
+        return scaleMatrix;
     }
 
     @Override
     public Vector3 transform(Vector3 v) {
         return new Vec3(
-                Sx * v.x(),
-                Sy * v.y(),
-                Sz * v.z());
+                scaleMatrix.get(R0, C0) * v.x(),
+                scaleMatrix.get(R1, C1) * v.y(),
+                scaleMatrix.get(R2, C2) * v.z());
     }
 
     @Override
@@ -43,11 +45,11 @@ public class Scaling implements AffineTransformation {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Scaling scaling = (Scaling) o;
-        return Float.compare(Sx, scaling.Sx) == 0 && Float.compare(Sy, scaling.Sy) == 0 && Float.compare(Sz, scaling.Sz) == 0;
+        return Objects.equals(scaleMatrix, scaling.scaleMatrix);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Sx, Sy, Sz);
+        return Objects.hash(scaleMatrix);
     }
 }
--- src/main/java/ru/vsu/cs/konygina_d/render_engine/Translator.java
+++ src/main/java/ru/vsu/cs/konygina_d/render_engine/Translator.java
@@ -2,40 +2,42 @@ package ru.vsu.cs.konygina_d.render_engine;
 
 import io.github.alphameo.linear_algebra.mat.Mat4;
 import io.github.alphameo.linear_algebra.mat.Matrix4;
+import static io.github.alphameo.linear_algebra.mat.Matrix4Col.*;
+import static io.github.alphameo.linear_algebra.mat.Matrix4Row.*;
 import io.github.alphameo.linear_algebra.vec.Vec3;
 import io.github.alphameo.linear_algebra.vec.Vector3;
 import java.util.Objects;
 
 public class Translator implements AffineTransformation {
-    private final float Tx, Ty, Tz;
+    private final Matrix4 translateMatrix;
 
     public Translator(float tx, float ty, float tz) {
-        Tx = tx;
-        Ty = ty;
-        Tz = tz;
+        this.translateMatrix = new Mat4(
+                1, 0, 0, tx,
+                0, 1, 0, ty,
+                0, 0, 1, tz,
+                0, 0, 0, 1);
     }
 
     public Translator() {
-        Tx = 0;
-        Ty = 0;
-        Tz = 0;
+        this.translateMatrix = new Mat4(
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1);
     }
 
     @Override
     public Matrix4 getMatrix() {
-        return new Mat4(
-                1, 0, 0, Tx,
-                0, 1, 0, Ty,
-                0, 0, 1, Tz,
-                0, 0, 0, 1);
+        return translateMatrix;
     }
 
     @Override
     public Vector3 transform(Vector3 v) {
         return new Vec3(
-                Tx + v.x(),
-                Ty + v.y(),
-                Tz + v.z());
+                translateMatrix.get(R3, C0) + v.x(),
+                translateMatrix.get(R3, C1) + v.y(),
+                translateMatrix.get(R3, C2) + v.z());
     }
 
     @Override
@@ -43,12 +45,12 @@ public class Translator implements AffineTransformation {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Translator that = (Translator) o;
-        return Float.compare(Tx, that.Tx) == 0 && Float.compare(Ty, that.Ty) == 0 && Float.compare(Tz, that.Tz) == 0;
+        return Objects.equals(translateMatrix, that.translateMatrix);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Tx, Ty, Tz);
+        return Objects.hash(translateMatrix);
     }
 }
 
