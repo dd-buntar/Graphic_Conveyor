43cb0e00f245dba6916f69bb84ba5a3167a831a3
IA1phaI <pavel.2014.play@gmail.com>
2024-12-24 16:46:28 +0300

    chore: add transitive opt to lib in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module ru.vsu.cs.konygina_d.render_engine {
-    requires io.github.alphameo.linear_algebra;
+    requires transitive io.github.alphameo.linear_algebra;
 
     exports ru.vsu.cs.konygina_d.render_engine;
 }
