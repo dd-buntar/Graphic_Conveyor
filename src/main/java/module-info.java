f204dd23c6620ba5470a2f7fb9ea13be1827a074
IA1phaI <pavel.2014.play@gmail.com>
2024-12-26 10:30:02 +0300

    refactor: change package name to affine

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,2 @@
-module ru.vsu.cs.konygina_d.render_engine {
-    requires transitive io.github.alphameo.linear_algebra;
-
-    exports ru.vsu.cs.konygina_d.render_engine;
+module $MODULE_NAME$ {
 }
--- src/main/java/ru/vsu/cs/konygina_d/affine/DataList.java
+++ src/main/java/ru/vsu/cs/konygina_d/affine/DataList.java
@@ -1,9 +1,13 @@
-package ru.vsu.cs.konygina_d.render_engine;
+package ru.vsu.cs.konygina_d.affine;
 
 public interface DataList<T> {
     void add(AffineTransformation at);
+
     void remove(int index);
+
     void remove(AffineTransformation at);
+
     void set(int index, AffineTransformation at);
+
     T get(int index);
 }
